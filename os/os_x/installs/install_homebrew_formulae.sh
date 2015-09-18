9c08b90995cc9faa0dc1f42a7c6a957933a87f70
Michele Gerarduzzi <michelegera@users.noreply.github.com>
2015-09-18 11:12:43 +0200

    Start databases at login

--- os/os_x/installs/install_homebrew_formulae.sh
+++ os/os_x/installs/install_homebrew_formulae.sh
@@ -19,12 +19,20 @@ main() {
     brew_install 'imagemagick' 'imagemagick --with-webp'
     brew_install 'memcached' 'memcached'
     brew_install 'mongodb' 'mongodb'
-    brew_install 'node' 'node'
     brew_install 'mysql' 'mysql'
+    brew_install 'node' 'node'
     brew_install 'sphinx' 'sphinx'
     brew_install 'tig' 'tig'
     brew_install 'tree' 'tree'
     brew_install 'watchman' 'watchman'
+
+    MYSQL_PREFIX="$(brew --prefix mysql)"
+    execute 'ln -sfv $MYSQL_PREFIX/*.plist ~/Library/LaunchAgents' \
+      'Start MySQL at login'
+
+    MONGODB_PREFIX="$(brew --prefix mongodb)"
+    execute 'ln -sfv $MONGODB_PREFIX/*.plist ~/Library/LaunchAgents' \
+      'Start MongoDB at login'
 
   fi
 
