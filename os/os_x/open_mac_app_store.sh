c5ed076e47fd552ab05734ce330aa372768e6e68
Michele Gerarduzzi <michelegera@users.noreply.github.com>
2015-09-21 10:43:09 +0200

    Add script to open MAS and suggest apps to install

---
+++ os/os_x/open_mac_app_store.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+cd "$(dirname "$BASH_SOURCE")" \
+  && source '../utils.sh'
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+declare -a APPS=(
+  'AppSana'
+  'Byword'
+  'Characters'
+  'Deliveries'
+  'Keynote'
+  'Sip'
+  'Tweetbot'
+  'VidConvert'
+  'XCode'
+)
+
+main() {
+
+  local MAS_APPLICATION=/Applications/App\ Store.app
+
+  open "$MAS_APPLICATION" --background &> /dev/null
+
+  print_in_purple 'Please install the following apps:\n'
+  for app in ${APPS[@]}; do
+    print_in_purple "  * $app\n"
+  done
+  printf '\n'
+
+  print_result $? 'Install Mac App Store applications'
+
+}
+
+main
