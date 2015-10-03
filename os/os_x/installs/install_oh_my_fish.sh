e67b80c4c431348629c60d1f3f5470d676a29e80
Michele Gerarduzzi <michelegera@users.noreply.github.com>
2015-10-03 16:35:46 +0200

    Pipe OMF install script to fish shell
    Close #12

--- os/os_x/installs/install_oh_my_fish.sh
+++ os/os_x/installs/install_oh_my_fish.sh
@@ -11,7 +11,7 @@ main() {
   if ! cmd_exists 'omf'; then
 
     curl -LsS github.com/oh-my-fish/oh-my-fish/raw/master/bin/install | \
-      CI=true sh &> /dev/null
+      CI=true fish &> /dev/null
     #   └─ simulate a CI environment, see http://git.io/vnu8b
 
     fish -c "omf install" &> /dev/null
