6f6000a565f2ece4c172854ab38345aa735d6745
Michele Gerarduzzi <michelegera@users.noreply.github.com>
2015-11-28 14:42:41 +0100

    Remove A Better Finder Rename

--- os/os_x/installs/install_homebrew_alternate_casks.sh
+++ os/os_x/installs/install_homebrew_alternate_casks.sh
@@ -15,7 +15,6 @@ main() {
     && brew_tap 'caskroom/versions'; then
 
     brew_install 'Sublime Text 3' 'sublime-text3' 'cask'
-    brew_install 'A Better Finder Rename 10 Beta' 'a-better-finder-rename-beta' 'cask'
 
   fi
 
