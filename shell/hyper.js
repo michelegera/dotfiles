3d85b747211ef9afe30f90e29915e741883760a8
Michele Gerarduzzi <michelegera@users.noreply.github.com>
2017-01-06 19:13:59 +0100

    Change Hyper default font
    This font slightly decreases the line height.

--- shell/hyper.js
+++ shell/hyper.js
@@ -4,7 +4,7 @@ module.exports = {
     fontSize: 14,
 
     // font family with optional fallbacks
-    fontFamily: '"Meslo LG L DZ for Powerline", monospace',
+    fontFamily: '"Meslo LG M DZ for Powerline", monospace',
 
     // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
     cursorColor: 'rgba(248,28,229,0.8)',
