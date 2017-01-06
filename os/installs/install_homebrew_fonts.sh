d65a182612f6cbac20c9c6ba2ab8760d52e071b5
Michele Gerarduzzi <michelegera@users.noreply.github.com>
2017-01-06 19:12:46 +0100

    Clean up unused fonts

--- os/installs/install_homebrew_fonts.sh
+++ os/installs/install_homebrew_fonts.sh
@@ -16,11 +16,9 @@ main() {
 
     brew_install 'Asap' 'font-asap' 'cask'
     brew_install 'Hack' 'font-hack' 'cask'
-    brew_install 'Inconsolata DZ' 'font-inconsolata-dz' 'cask'
-    brew_install 'Input' 'font-input' 'cask'
     brew_install 'Meslo LG for Powerline' 'font-meslo-lg-for-powerline' 'cask'
-    brew_install 'Mononoki' 'font-mononoki' 'cask'
     brew_install 'Montserrat' 'font-montserrat' 'cask'
+    brew_install 'Open Sans' 'font-open-sans' 'cask'
 
   fi
 
