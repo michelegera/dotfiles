88ca8f14b9a0a674965842d3b0281b18c1224272
Michele Gerarduzzi <michelegera@users.noreply.github.com>
2017-01-18 17:12:47 +0100

    Hide output of nvm installation

--- os/installs/install_nvm_and_plugins.sh
+++ os/installs/install_nvm_and_plugins.sh
@@ -9,7 +9,7 @@ cd "$(dirname "$BASH_SOURCE")" \
 main() {
 
   # nvm
-  bash -c "$(curl -LsS https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh)"
+  bash -c "$(curl -LsS https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh)" &> /dev/null
 
   # Plugins
   if [ -d '~/.oh-my-zsh/' ]; then
