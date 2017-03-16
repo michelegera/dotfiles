22b535d20fe67aed0404166a97794712788ad6ab
Michele Gerarduzzi <michelegera@users.noreply.github.com>
2017-03-16 12:20:05 +0100

    Update Transmission preferences

--- os/preferences/set_transmission_preferences.sh
+++ os/preferences/set_transmission_preferences.sh
@@ -10,8 +10,22 @@ set_transmission_preferences() {
   execute 'defaults write org.m0k.transmission DeleteOriginalTorrent -bool true' \
     'Delete the original torrent files'
 
-  execute 'defaults write org.m0k.transmission DownloadAsk -bool false' \
+  execute 'defaults write org.m0k.transmission DownloadAsk -bool false &&
+           defaults write org.m0k.transmission MagnetOpenAsk -bool false' \
     'Don’t prompt for confirmation before downloading'
+
+  # Source: https://giuliomac.wordpress.com/2014/02/19/best-blocklist-for-transmission/
+  execute 'defaults write org.m0k.transmission BlocklistURL -string "http://john.bitsurge.net/public/biglist.p2p.gz"' \
+    'Use a blocklist'
+
+  execute 'defaults write org.m0k.transmission BlocklistAutoUpdate -bool true' \
+    'Auto-update blocklist'
+
+  execute 'defaults write org.m0k.transmission RandomPort -bool true' \
+    'Randomize port on launch'
+
+  execute 'defaults write org.m0k.transmission CheckRemoveDownloading -bool true' \
+    'Don’t prompt for confirmation before removing non-downloading active transfers'
 
   execute 'defaults write org.m0k.transmission WarningDonate -bool false' \
     'Hide the donate message'
