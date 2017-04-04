f10f1ed4761e68d9b3d3c878fb74f2a7df6c17c5
Michele Gerarduzzi <michelegera@users.noreply.github.com>
2017-04-04 15:32:52 +0200

    Clean up installed fonts

--- os/installs/install_homebrew_fonts.sh
+++ os/installs/install_homebrew_fonts.sh
@@ -14,11 +14,7 @@ main() {
   if cmd_exists 'brew' \
     && brew_tap 'caskroom/fonts'; then
 
-    brew_install 'Asap' 'font-asap' 'cask'
-    brew_install 'Hack' 'font-hack' 'cask'
-    brew_install 'Meslo LG for Powerline' 'font-meslo-lg-for-powerline' 'cask'
-    brew_install 'Montserrat' 'font-montserrat' 'cask'
-    brew_install 'Open Sans' 'font-open-sans' 'cask'
+    brew_install 'Source Code Pro for Powerline' 'font-source-code-pro-for-powerline' 'cask'
 
   fi
 
