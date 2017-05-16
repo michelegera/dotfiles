b3819a291a9c0f8492e95de0c631174df4a9bb76
Michele Gerarduzzi <michelegera@users.noreply.github.com>
2017-05-16 14:41:40 +0200

    Fix Oh My Zsh installation
    Remove Spaceship theme installation as it seems to have issues with this setup.

--- os/installs/install_oh_my_zsh.sh
+++ os/installs/install_oh_my_zsh.sh
@@ -10,13 +10,9 @@ main() {
 
   # Install Oh My Zsh
   # https://github.com/robbyrussell/oh-my-zsh
-  curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh &> /dev/null
+  bash -c "$(curl -LsS https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" &> /dev/null
 
-  # Install Spaceship theme
-  # https://github.com/denysdovhan/spaceship-zsh-theme
-  curl -LsSo - https://raw.githubusercontent.com/denysdovhan/spaceship-zsh-theme/master/install.zsh | zsh &> /dev/null
-
-  print_result $? 'Install Oh My Zsh and theme'
+  print_result $? 'Install Oh My Zsh'
 
 }
 
