c6367b142326d999b3c3d663cf62b0a37b53eb6d
Michele Gerarduzzi <michelegera@users.noreply.github.com>
2017-08-07 22:07:25 +0200

    Symlink Spectacles settings
    Spectacles now conveniently serializes shortcuts in a JSON file that we can symlink.

---
+++ os/create_settings_symbolic_links.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+cd "$(dirname "$BASH_SOURCE")" \
+  && source 'utils.sh'
+
+declare -a FILES_TO_SYMLINK=(
+
+  'settings/Spectacles'
+
+)
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+main() {
+
+  local i=''
+  local sourceFile=''
+  local targetFile=''
+
+  for i in ${FILES_TO_SYMLINK[@]}; do
+
+    sourceFile="$(cd .. && pwd)/$i"
+    targetFile="$HOME/Library/Application\ Support/$(printf "%s" "$i" | sed "s/.*\/\(.*\)/\1/g")"
+
+    if [ -e "$targetFile" ]; then
+      if [ "$(readlink "$targetFile")" != "$sourceFile" ]; then
+
+        ask_for_confirmation "'$targetFile' already exists, do you want to overwrite it?"
+        if answer_is_yes; then
+          rm -rf "$targetFile"
+          execute "ln -fs $sourceFile $targetFile" "$targetFile → $sourceFile"
+        else
+          print_error "$targetFile → $sourceFile"
+        fi
+
+      else
+        print_success "$targetFile → $sourceFile"
+      fi
+    else
+      execute "ln -fs $sourceFile $targetFile" "$targetFile → $sourceFile"
+    fi
+
+  done
+
+}
+
+main
