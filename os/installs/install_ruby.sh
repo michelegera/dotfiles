ad08ebcd1f5781b5481b84572622bffbfbb5d334
Michele Gerarduzzi <michelegera@users.noreply.github.com>
2017-08-07 22:27:55 +0200

    Install Ruby with OpenSSL support
    Closes #21

--- os/installs/install_ruby.sh
+++ os/installs/install_ruby.sh
@@ -10,6 +10,8 @@ main() {
 
   if cmd_exists 'rbenv'; then
 
+    export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$(brew --prefix openssl)"
+
     # Find the most recent stable MRI version number, see http://stackoverflow.com/a/30183040/246054
     local RUBY_VERSION="$(rbenv install -l | sed -n '/^[[:space:]]*[0-9]\{1,\}\.[0-9]\{1,\}\.[0-9]\{1,\}[[:space:]]*$/ h;${g;p;}')"
 
