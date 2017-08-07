432f5e5f916945b5903d430aaa890e13c3bb2dca
Michele Gerarduzzi <michelegera@users.noreply.github.com>
2017-08-07 22:36:51 +0200

    Disable guest access
    Closes #1

--- os/preferences/set_misc_preferences.sh
+++ os/preferences/set_misc_preferences.sh
@@ -10,6 +10,10 @@ set_misc_preferences() {
   execute 'defaults write com.apple.ImageCapture disableHotPlug -bool true' \
     'Prevent ImageCapture from opening automatically'
 
+  execute 'defaults write com.apple.AppleFileServer guestAccess -bool false;
+           defaults write com.apple.smb.server AllowGuestAccess -bool false' \
+    'Disable guest access'
+
   execute 'sudo systemsetup -setremotelogin on' \
     'Enable remote login'
 
