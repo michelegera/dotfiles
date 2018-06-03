8c60dbd35ec81271878589e538abe12a98f7823f
Michele Gerarduzzi <michelegera@users.noreply.github.com>
2018-06-04 00:48:07 +0200

    Add Visual Studio Code

--- os/installs/install_homebrew_casks.sh
+++ os/installs/install_homebrew_casks.sh
@@ -50,6 +50,7 @@ main() {
     brew_install 'Transmission' 'transmission' 'cask'
     brew_install 'Transmit' 'transmit' 'cask'
     brew_install 'Viscosity' 'viscosity' 'cask'
+    brew_install 'Visual Studio Code' 'visual-studio-code' 'cask'
     brew_install 'VLC' 'vlc' 'cask'
     brew_install 'WhatsApp' 'whatsapp' 'cask'
     brew_install 'xScope' 'xscope' 'cask'
