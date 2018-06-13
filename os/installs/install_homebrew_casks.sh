c81cf3ba964b6e21c4b79bc4c72b4af8e5ed6e24
Michele Gerarduzzi <michelegera@users.noreply.github.com>
2018-06-14 00:09:29 +0200

    Clean up casks
    Only install apps that are used on a daily basis or so.

--- os/installs/install_homebrew_casks.sh
+++ os/installs/install_homebrew_casks.sh
@@ -24,29 +24,21 @@ main() {
     brew_install 'AppCleaner' 'appcleaner' 'cask'
     brew_install 'AppGrid' 'appgrid' 'cask'
     brew_install 'Arq' 'arq' 'cask'
-    brew_install 'Cocktail' 'cocktail' 'cask'
-    brew_install 'Dropbox' 'dropbox' 'cask'
-    brew_install 'Evernote' 'evernote' 'cask'
+    brew_install 'Cocktail' 'cocktail' 'cask' 
     brew_install 'Firefox' 'firefox' 'cask'
-    brew_install 'FontBase' 'fontbase' 'cask'
-    brew_install 'Google Chrome' 'google-chrome' 'cask'
     brew_install 'Google Backup & Sync' 'google-backup-and-sync' 'cask'
+    brew_install 'Google Chrome' 'google-chrome' 'cask'
     brew_install 'ImageOptim' 'imageoptim' 'cask'
     brew_install 'Install Disk Creator' 'install-disk-creator' 'cask'
-    brew_install 'iStat Menus' 'istat-menus' 'cask'
     brew_install 'iTerm2' 'iterm2' 'cask'
-    brew_install 'Limechat' 'limechat' 'cask'
     brew_install 'Little Snitch' 'little-snitch' 'cask'
-    brew_install 'Numi' 'numi' 'cask'
+    brew_install 'Microsoft Office' 'microsoft-office' 'cask'
     brew_install 'Paw' 'paw' 'cask'
     brew_install 'Postico' 'postico' 'cask'
     brew_install 'Sequel Pro' 'sequel-pro' 'cask'
-    brew_install 'Skitch' 'skitch' 'cask'
-    brew_install 'Skype' 'skype' 'cask'
     brew_install 'Slack' 'slack' 'cask'
-    brew_install 'Source Code Pro for Powerline' 'font-source-code-pro-for-powerline' 'cask'
-    brew_install 'Spotify' 'spotify' 'cask'
     brew_install 'Spotify Notifications' 'spotify-notifications' 'cask'
+    brew_install 'Spotify' 'spotify' 'cask'
     brew_install 'Sublime Text' 'sublime-text' 'cask'
     brew_install 'Telegram Desktop' 'telegram-desktop' 'cask'
     brew_install 'The Unarchiver' 'the-unarchiver' 'cask'
@@ -55,18 +47,11 @@ main() {
     brew_install 'Viscosity' 'viscosity' 'cask'
     brew_install 'VLC' 'vlc' 'cask'
     brew_install 'WhatsApp' 'whatsapp' 'cask'
-    brew_install 'xScope' 'xscope' 'cask'
     brew_install 'Zoom.us' 'zoomus' 'cask'
     printf '\n'
 
     print_info 'Please run the Adobe Creative Cloud installer'
     print_info 'Please run the Little Snitch installer'
-
-    ask_for_confirmation 'Do you want to install Microsoft Office?'
-
-    if answer_is_yes; then
-      brew_install 'Microsoft Office' 'microsoft-office' 'cask'
-    fi
   fi
 
 }
