1156f39ba3311289cd356d3bbaff4b53c60a3102
Michele Gerarduzzi <michelegera@users.noreply.github.com>
2018-09-10 22:31:49 +0200

    Move default apps setting after symlinking
    This is done in order to have `.duti` available.

--- os/install_applications.sh
+++ os/install_applications.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd "$(dirname "$BASH_SOURCE")" \
-    && source 'utils.sh'
+  && source 'utils.sh'
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
@@ -9,7 +9,6 @@ main() {
 
   ./installs/main.sh
   ./change_default_shell.sh
-  ./set_default_apps.sh
   ./start_apps_at_login.sh
 
 }
