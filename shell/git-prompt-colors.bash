d9b90ca64d4c2ffb3be9161c97f7173ff07c0642
Michele Gerarduzzi <michelegera@users.noreply.github.com>
2019-02-22 00:13:02 +0100

    Handle missing Node executable in prompt

--- shell/git-prompt-colors.bash
+++ shell/git-prompt-colors.bash
@@ -2,19 +2,21 @@ override_git_prompt_colors() {
   GIT_PROMPT_THEME_NAME="Spaceship"
 
   if [ -e ~/.rvm/bin/rvm-prompt ]; then
-    RubyPrompt='⬥ v$(~/.rvm/bin/rvm-prompt v g)'
+    RubyPrompt=' ⬥ v$(~/.rvm/bin/rvm-prompt v g)'
   else
-    RubyPrompt='⬥ v$(ruby -v | sed -E "s/ruby ([0-9]+\.[0-9]+\.[0-9]+).*/\1/")'
+    RubyPrompt=' ⬥ v$(ruby -v | sed -E "s/ruby ([0-9]+\.[0-9]+\.[0-9]+).*/\1/")'
   fi
 
-  NodePrompt='⬢ $(node -v)'
+  if [ -x "$(command -v node)" ]; then
+    NodePrompt=' ⬢ $(node -v)'
+  fi
 
   GIT_PROMPT_START_USER="\n${Blue}${PathShort}"
   GIT_PROMPT_PREFIX="${ResetColor}on ${Magenta} "
   GIT_PROMPT_BRANCH="${Magenta}"
   GIT_PROMPT_SEPARATOR="${ResetColor} ${BrightBlack}["
   GIT_PROMPT_SUFFIX="${BrightBlack}]"
-  GIT_PROMPT_END_USER=" ${ResetColor}via ${Green}${NodePrompt} ${Red}${RubyPrompt}${ResetColor}\n_LAST_COMMAND_INDICATOR_${ResetColor} "
+  GIT_PROMPT_END_USER=" ${ResetColor}via${Green}${NodePrompt}${Red}${RubyPrompt}${ResetColor}\n_LAST_COMMAND_INDICATOR_${ResetColor} "
 
   GIT_PROMPT_CLEAN="${Green}✔"
   GIT_PROMPT_STAGED="${Green}+"
