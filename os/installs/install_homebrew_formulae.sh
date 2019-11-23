0da3c57dc2772702e7fa4a52285eda6f14740a03
Michele Gerarduzzi <michelegera@users.noreply.github.com>
2019-11-23 18:46:41 +0100

    Update bash-completion to v2

--- os/installs/install_homebrew_formulae.sh
+++ os/installs/install_homebrew_formulae.sh
@@ -14,7 +14,7 @@ main() {
   if cmd_exists 'brew' \
     && brew_tap 'heroku/brew'; then
 
-    brew_install 'bash-completion' 'bash-completion'
+    brew_install 'bash-completion@2' 'bash-completion@2'
     brew_install 'bash' 'bash'
     brew_install 'bat' 'bat'
     brew_install 'cmake' 'cmake'
