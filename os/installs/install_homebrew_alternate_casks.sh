5c5dccf498ee41e8fd8ab4ca04a20a7f39e58b70
Michele Gerarduzzi <hello@michelegera.dev>
2020-06-23 12:09:06 +0200

    Remove Sequel Pro, add TablePlus
    Sequel Pro is not actively maintained anymore and its fork, Sequel Ace,
    seems a bit rough/unpolished (and it has a hideous icon).
    
    Switch to TablePlus which supports multiple databases and get rid of
    Postico too.

--- os/installs/install_homebrew_alternate_casks.sh
+++ os/installs/install_homebrew_alternate_casks.sh
@@ -15,7 +15,6 @@ main() {
     && brew_tap 'homebrew/cask-versions'; then
 
     brew_install 'Java 1.8' 'adoptopenjdk8' 'cask' # Required by elasticsearch
-    brew_install 'Sequel Pro (Nightly Build)' 'sequel-pro-nightly' 'cask'
 
   fi
 
