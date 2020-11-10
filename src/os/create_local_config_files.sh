ad17afd04a750737efe9a1cc6c2929f378933e6d
Michele Gerarduzzi <hello@michelegera.dev>
2020-11-10 15:38:59 +0100

    Add local Fish configuration creation

--- src/os/create_local_config_files.sh
+++ src/os/create_local_config_files.sh
@@ -56,12 +56,32 @@ create_sshconfig_local() {
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  - - - - - -
 
+create_fishconfig_local() {
+
+    declare -r FILE_PATH="$HOME/.fishconfig.local"
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    if [ ! -e "$FILE_PATH" ] || [ -z "$FILE_PATH" ]; then
+
+        printf "%s\n" \
+"# set NPMJS_AUTH_TOKEN
+# set REACT_APP_NPMJS_AUTH_TOKEN \$NPMJS_AUTH_TOKEN" \
+        >> "$FILE_PATH"
+    fi
+
+    print_result $? "$FILE_PATH"
+}
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  - - - - - -
+
 main() {
 
     print_in_purple "\n • Create local config files\n\n"
 
     create_gitconfig_local
     create_sshconfig_local
+    create_fishconfig_local
 
 }
 
