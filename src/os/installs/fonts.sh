0fa55b5fb20f62b1e9f8ce9753e30b8a5ead1a7e
Michele Gerarduzzi <hello@michelegera.dev>
2020-11-12 21:27:52 +0100

    Install `svn` dependency for Homebrew fonts

--- src/os/installs/fonts.sh
+++ src/os/installs/fonts.sh
@@ -4,8 +4,14 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
     && . "../../utils.sh" \
     && . "./utils.sh"
 
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 print_in_purple "\n   Fonts\n\n"
+
+# Subversion is required by the `font-source-code-pro-for-powerline` cask
+
+brew_install "Subversion (requirement)" "svn"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 brew_install "Source Code Pro for Powerline" "font-source-code-pro-for-powerline" "homebrew/cask-fonts" "cask"
