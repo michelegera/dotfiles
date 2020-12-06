43df3b96c7d6cb03ec1b006fc25825e300802860
Michele Gerarduzzi <hello@michelegera.dev>
2020-12-06 16:50:00 +0100

    Do not minimize windows to application icons

--- src/os/preferences/dock.sh
+++ src/os/preferences/dock.sh
@@ -13,9 +13,6 @@ execute "defaults write com.apple.dock expose-animation-duration -float 0.1" \
 execute "defaults write com.apple.dock expose-group-by-app -bool false" \
     "Do not group windows by application in Mission Control"
 
-execute "defaults write com.apple.dock minimize-to-application -bool true" \
-    "Reduce clutter by minimizing windows into their application icons"
-
 execute "defaults write com.apple.dock mru-spaces -bool false" \
     "Do not automatically rearrange spaces based on most recent use"
 
