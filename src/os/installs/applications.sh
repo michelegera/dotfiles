8b6ccef59c9ab0c84b7f49478f1cbdd9b40009a8
Michele Gerarduzzi <hello@michelegera.dev>
2022-08-27 13:40:46 +0200

    Update installed apps
    - Remove MS Office and Garmin Express
    - Add Affinity apps

--- src/os/installs/applications.sh
+++ src/os/installs/applications.sh
@@ -8,13 +8,6 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 
 print_in_purple "\n   Applications\n\n"
 
-# The following apps will have to be installed manually as they are not (and
-# might never be) in homebrew/cask
-#
-# - Affinity Designer
-# - Affinity Photo
-# - Affinity Publisher
-
 brew_install "1Password" "1password" "--cask"
 brew_install "A Better Finder Rename" "a-better-finder-rename" "--cask"
 brew_install "Alacritty" "alacritty" "--cask"
@@ -23,10 +16,8 @@ brew_install "Arq" "arq" "--cask"
 brew_install "Asana" "asana" "--cask"
 brew_install "balenaEtcher" "balenaetcher" "--cask"
 brew_install "Catch" "catch" "--cask"
-brew_install "Garmin Express" "garmin-express" "--cask" "homebrew/cask-drivers"
 brew_install "Google Drive" "google-drive" "--cask"
 brew_install "Install Disk Creator" "install-disk-creator" "--cask"
-brew_install "Microsoft Office" "microsoft-office" "--cask"
 brew_install "NordVPN" "nordvpn" "--cask"
 brew_install "NTFS for Mac" "paragon-ntfs" "--cask"
 brew_install "Raycast" "raycast" "--cask"
--- src/os/installs/creative_tools.sh
+++ src/os/installs/creative_tools.sh
@@ -8,6 +8,9 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 
 print_in_purple "\n   Creative Tools\n\n"
 
+brew_install "Affinity Designer" "affinity-designer" "--cask"
+brew_install "Affinity Photo" "affinity-photo" "--cask"
+brew_install "Affinity Publisher" "affinity-publisher" "--cask"
 brew_install "Figma" "figma" "--cask"
 brew_install "Handbrake" "handbrake" "--cask"
 brew_install "Kap" "kap" "--cask"
