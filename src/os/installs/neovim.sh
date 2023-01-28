76569e90f77b0ad0f1614df637585505e0539158
Michele Gerarduzzi <hello@michelegera.dev>
2023-01-28 18:15:00 +0100

    Switch to LunarVim

--- src/os/installs/neovim.sh
+++ src/os/installs/neovim.sh
@@ -5,12 +5,23 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
+install_lunarvim() {
+
+    local script="https://raw.githubusercontent.com/lunarvim/lunarvim/fc6873809934917b470bff1b072171879899a36b/utils/installer/install.sh"
+
+    LV_BRANCH='release-1.2/neovim-0.8' INTERACTIVE_MODE=0 /bin/bash -c "$(curl -s $script)" &> /dev/null
+
+    print_result $? "Install LunarVim"
+
+}
 
 main() {
 
     print_in_purple "\n   Neovim\n\n"
 
     brew_install "Neovim" "neovim"
+
+    install_lunarvim
 
 }
 
