5f344fd227122c8548397664ae14aba7f22787d7
Michele Gerarduzzi <hello@michelegera.dev>
2023-02-17 19:51:50 +0100

    Switch to WezTerm

---
+++ src/shell/config/wezterm/wezterm.lua
@@ -0,0 +1,20 @@
+local wezterm = require 'wezterm'
+local act = wezterm.action
+
+return {
+	-- Hide tab bar, since we use tmux
+	enable_tab_bar = false,
+
+	-- Remove vertical padding
+	window_padding = {
+    top = 0,
+    bottom = 0,
+  },
+
+	-- Use a nerd font
+	font = wezterm.font 'MonoLisa Nerd Font',
+	font_size = 13.0,
+
+	-- Set color scheme
+	color_scheme = "Catppuccin Macchiato", -- or Mocha, Frappe, Latte
+}
