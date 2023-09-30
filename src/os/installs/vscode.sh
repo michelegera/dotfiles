42d4274e9aff4f67b499db218b28705f8152652d
Michele Gerarduzzi <hello@michelegera.dev>
2023-09-30 09:09:39 +0200

    Fix VS Code settings installation

--- src/os/installs/vscode.sh
+++ src/os/installs/vscode.sh
@@ -20,6 +20,7 @@ brew_install "Visual Studio Code" "visual-studio-code"
 printf "\n"
 
 # Symlink settings
+mkdir -p "${HOME}/Library/Application\ Support/Code/User/"
 symlink "$(realpath ~/.vscode)/settings.json" \
     "${HOME}/Library/Application\ Support/Code/User/settings.json"
 
