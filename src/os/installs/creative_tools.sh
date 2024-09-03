87d0fb751530e2bed66fbaadc8e5c1ddb524fda4
Michele Gerarduzzi <hello@michelegera.dev>
2024-09-03 08:59:57 +0200

    Remove `homebrew/cask-versions` tap

--- src/os/installs/creative_tools.sh
+++ src/os/installs/creative_tools.sh
@@ -8,8 +8,6 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 
 print_in_purple "\n   Creative Tools\n\n"
 
-brew_tap "homebrew/cask-versions"
-
 brew_install "Affinity Designer" "affinity-designer"
 brew_install "Affinity Photo" "affinity-photo"
 brew_install "Affinity Publisher" "affinity-publisher"
