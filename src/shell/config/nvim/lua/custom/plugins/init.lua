d850162944b5323502076f84cc910b16927003cf
Michele Gerarduzzi <hello@michelegera.dev>
2024-10-27 17:46:54 +0100

    Add Comment.nvim plugin

--- src/shell/config/nvim/lua/custom/plugins/init.lua
+++ src/shell/config/nvim/lua/custom/plugins/init.lua
@@ -2,4 +2,12 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    "numToStr/Comment.nvim",
+    opts = {},
+    confing = function()
+      require("Comment").setup()
+    end,
+  },
+}
