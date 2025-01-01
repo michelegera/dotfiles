b38b91fb68b17d127b8c538933945a31c29cc3e6
Michele Gerarduzzi <hello@michelegera.dev>
2025-01-01 19:18:55 +0100

    Remove rarely used applications

--- src/os/installs/communications.sh
+++ src/os/installs/communications.sh
@@ -9,7 +9,6 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 print_in_purple "\n   Communications\n\n"
 
 brew_install "Discord" "discord"
-brew_install "Notion" "notion"
 brew_install "Slack" "slack"
 brew_install "Telegram Desktop" "telegram-desktop"
 brew_install "WhatsApp" "whatsapp"
