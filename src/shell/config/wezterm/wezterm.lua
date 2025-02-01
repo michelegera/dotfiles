77c81826ca34ee98f9ddba4ffea008eb9a079b2d
Michele Gerarduzzi <hello@michelegera.dev>
2025-02-01 17:15:47 +0100

    Use spaces for WezTerm’s configuration indentation

--- src/shell/config/wezterm/wezterm.lua
+++ src/shell/config/wezterm/wezterm.lua
@@ -1,21 +1,21 @@
 local wezterm = require 'wezterm'
 
 return {
-	-- Hide tab bar, since we use tmux
-	enable_tab_bar = false,
+  -- Hide tab bar, since we use tmux
+  enable_tab_bar = false,
 
-	-- Remove vertical padding
-	window_padding = {
+  -- Remove vertical padding
+  window_padding = {
     top = 0,
     bottom = 0,
   },
 
-	-- Use a nerd font
-	font = wezterm.font 'MonoLisa Nerd Font',
-	font_size = 12.0,
+  -- Use a nerd font
+  font = wezterm.font 'MonoLisa Nerd Font',
+  font_size = 12.0,
 
-	-- Set color scheme
-	color_scheme = "Tokyo Night",
+  -- Set color scheme
+  color_scheme = "Tokyo Night",
 
   -- Set cursor style
   default_cursor_style = "BlinkingUnderline",
