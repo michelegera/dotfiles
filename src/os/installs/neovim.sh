4406df29b9dcb6a11d76c5c5e6f7ac0cd6e731a5
Michele Gerarduzzi <hello@michelegera.dev>
2025-02-07 19:46:44 +0100

    Remove kickstart.nvim

--- src/os/installs/neovim.sh
+++ src/os/installs/neovim.sh
@@ -5,27 +5,11 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-install_lazyvim() {
-    declare -r NVIM_DIRECTORY="$HOME/.config/nvim"
-
-    # Clone the repository into an non-empty directory
-    git -C "$NVIM_DIRECTORY" init &> /dev/null
-    git -C "$NVIM_DIRECTORY" remote add origin https://github.com/LazyVim/starter &> /dev/null
-    git -C "$NVIM_DIRECTORY" fetch &> /dev/null
-    git -C "$NVIM_DIRECTORY" checkout origin/main &> /dev/null
-    rm -rf "$NVIM_DIRECTORY/.git" &> /dev/null
-
-    print_result $? "Install LazyVim"
-
-}
-
 main() {
 
     print_in_purple "\n   Neovim\n\n"
 
     brew_install "Neovim" "neovim"
-
-    install_lazyvim
 
 }
 
